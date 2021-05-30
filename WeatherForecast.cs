542cd1077019f8418ae8a6908f2817746201ad66
Minh To <minh.to@hootsuite.com>
2021-05-30 14:38:31 -0400

    added an extra property to weatherForecast for fun

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -11,5 +11,7 @@ namespace pawsitive
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+        public string Sky { get; set; }
     }
 }
