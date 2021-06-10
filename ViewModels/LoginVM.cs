38402bbe1b59572ebfd79c86816f4aab3a56664e
Jihyun Lee <azurer@nate.com>
2021-06-10 00:38:57 -0400

    change username to email in login, add remember me option

--- ViewModels/LoginVM.cs
+++ ViewModels/LoginVM.cs
@@ -8,10 +8,23 @@ namespace pawsitive.ViewModels
 {
     public class LoginVM
     {
+        // Jihyun, 6/9, changed the name from UserName
         [Required(ErrorMessage = "User Name is required")]
-        public string Username { get; set; }
+        public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+
     }
+
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
 }
