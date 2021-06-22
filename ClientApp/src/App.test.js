71385df0cc4203ba65eef007346e4a0154f777a7
viet-quocnguyen <nguyenquocviet170898@gmail.com>
2021-06-21 20:57:07 -0400

    fix test file for react app

--- ClientApp/src/App.test.js
+++ ClientApp/src/App.test.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
 it('renders without crashing', async () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <MemoryRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </MemoryRouter>, div);
   await new Promise(resolve => setTimeout(resolve, 1000));
 });
