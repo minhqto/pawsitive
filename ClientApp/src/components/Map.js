2ed1c715ff590d5e1004434dc7b98e38c37bb884
Artur Felix <arturmontesanofelix@gmail.com>
2021-07-28 22:13:16 -0400

    Maps connected to back-end

--- ClientApp/src/components/Map.js
+++ ClientApp/src/components/Map.js
@@ -1,4 +1,5 @@
 ﻿import { Loader } from "@googlemaps/js-api-loader";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Map = (props) => {
@@ -15,42 +16,96 @@ const Map = (props) => {
     version: "weekly",
   });
 
+  /*geocode();
+
+  function geocode() {
+    var location = "22 Main St Boston MA"; //"26 Goodwood Park Cres East Toronto Canada";
+    axios
+      .get("https://maps.googleapis.com/maps/api/geocode/json", {
+        parans: {
+          address: location,
+          key: "AIzaSyC8eAELSwMhuVOWoWVDB1JFv1TioUaRAIA",
+        },
+      })
+      .then(function (response) {
+        console.log("Response: " + response);
+      })
+      .catch(function (err) {
+        console.log("Error: " + err);
+      });
+  }*/
+
+  var options = {
+    method: "GET",
+    url: "https://google-maps-geocoding.p.rapidapi.com/geocode/json",
+    params: { address: "", language: "en" },
+    headers: {
+      "x-rapidapi-key": "b45077c411msh3b11fddee5a5e95p114864jsn3d68c616da31",
+      "x-rapidapi-host": "google-maps-geocoding.p.rapidapi.com",
+    },
+  };
+
+  console.log("test");
+
   useEffect(() => {
     if (props.user != null) {
       props.user.forEach((user) => {
-        tourStops.push([
-          {
-            lat: parseInt(user.address.geo.lat),
-            lng: parseInt(user.address.geo.lng),
-          },
-          user.name,
-        ]);
-      });
-    }
-    if (tourStops.length > 0) {
-      loader
-        .load()
-        .then(() => {
-          const map = new window.google.maps.Map(
-            document.getElementById("map"),
-            {
-              center: tourStops[0][0],
-              zoom: 2,
+        options.params.address =
+          user.address.streetAddress +
+          ", " +
+          user.address.city +
+          ", " +
+          user.address.province +
+          ", " +
+          user.address.country;
+
+        axios
+          .request(options)
+          .then(function (response) {
+            tourStops.push([
+              {
+                lat: response.data.results[0].geometry.location.lat,
+                lng: response.data.results[0].geometry.location.lng,
+              },
+              user.firstName + " " + user.lastName,
+            ]);
+            //console.log(
+            //  "Lat: " +
+            //    response.data.results[0].geometry.location.lat.toString() +
+            //    " Lng: " +
+            //    response.data.results[0].geometry.location.lng.toString()
+            //);
+            if (tourStops.length == props.user.length) {
+              loader
+                .load()
+                .then(() => {
+                  const map = new window.google.maps.Map(
+                    document.getElementById("map"),
+                    {
+                      center: tourStops[0][0],
+                      zoom: 2,
+                    }
+                  );
+                  tourStops.forEach(([position, title], i) => {
+                    const marker = new window.google.maps.Marker({
+                      position,
+                      map,
+                      title: `${i + 1}. ${title}`,
+                      label: `${i + 1}`,
+                      optimized: false,
+                    });
+                  });
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
             }
-          );
-          tourStops.forEach(([position, title], i) => {
-            const marker = new window.google.maps.Marker({
-              position,
-              map,
-              title: `${i + 1}. ${title}`,
-              label: `${i + 1}`,
-              optimized: false,
-            });
+            //console.log(tourStops);
+          })
+          .catch(function (error) {
+            console.error(error);
           });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      });
     }
   }, [props]);
 
