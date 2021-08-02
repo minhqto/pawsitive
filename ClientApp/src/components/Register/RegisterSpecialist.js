030928b7cd5dc3b5041c05990806532cd5fcd524
Minh To <minhqto3@gmail.com>
2021-08-02 17:45:21 -0400

    pills!

--- ClientApp/src/components/Register/RegisterSpecialist.js
+++ ClientApp/src/components/Register/RegisterSpecialist.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
